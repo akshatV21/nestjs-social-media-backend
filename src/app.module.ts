fbe15831ea9025c85072ab0238e77ba9069d112b
akshatV21 <a.vishwakarma2110@gmail.com>
2023-11-20 12:10:58 +0530

    starter code added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        JWT_SECRET: Joi.string().required(),
+        MONGO_URI: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import helmet from 'helmet'
+import * as morgran from 'morgan'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService)
+
+  const PORT = configService.get('PORT')
+
+  app.use(helmet())
+  app.use(morgran('dev'))
+
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  await app.listen(PORT, () => console.log(`Listening to requests on port: ${PORT}`))
 }
 bootstrap();
