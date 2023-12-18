8a12cecae0f43769ebd1564ae0a42123cc5627a1
akshatV21 <a.vishwakarma2110@gmail.com>
2023-12-18 13:21:13 +0530

    Modified user model

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,8 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { hashSync } from 'bcrypt'
-import { Document } from 'mongoose'
+import { Document, Types } from 'mongoose'
 
 export type UserDocument = User & Document
+
+@Schema({ _id: false })
+export class UserRequests {
+  @Prop({ default: [], ref: 'Request' })
+  sent: Types.ObjectId[]
+
+  @Prop({ default: [], ref: 'Request' })
+  received: Types.ObjectId[]
+}
 
 @Schema({ timestamps: true })
 export class User {
@@ -14,6 +23,15 @@ export class User {
 
   @Prop({ required: true })
   password: string
+
+  @Prop({ default: [], ref: 'User' })
+  followers: Types.ObjectId[]
+
+  @Prop({ default: [], ref: 'User' })
+  following: Types.ObjectId[]
+
+  @Prop({ default: new UserRequests() })
+  requests: UserRequests
 }
 
 const UserSchema = SchemaFactory.createForClass(User)
