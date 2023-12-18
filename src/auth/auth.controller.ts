750ba953a71157dd21f9464ef890d5a8280175b2
akshatV21 <a.vishwakarma2110@gmail.com>
2023-12-18 13:03:29 +0530

    Added login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { RegisterDto } from './dtos/register.dto'
 import { HttpSuccessResponse } from 'src/utils/types'
+import { LoginDto } from './dtos/login.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,11 @@ export class AuthController {
   async httpRegister(@Body() registerDto: RegisterDto): HttpSuccessResponse {
     const user = await this.authService.register(registerDto)
     return { success: true, message: 'User registered successfully.', data: { user } }
+  }
+
+  @Post('login')
+  async httpLogin(@Body() loginDto: LoginDto): HttpSuccessResponse {
+    const res = await this.authService.login(loginDto)
+    return { success: true, message: 'User logged in successfully.', data: res }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,38 @@
 import { Injectable, BadRequestException } from '@nestjs/common'
 import { UserRepository } from 'src/database/repositories'
 import { RegisterDto } from './dtos/register.dto'
+import { LoginDto } from './dtos/login.dto'
+import { compareSync } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly UserRepository: UserRepository) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly UserRepository: UserRepository,
+  ) {}
 
   async register(registerDto: RegisterDto) {
     const emailExists = await this.UserRepository.exists({ email: registerDto.email })
     if (emailExists) throw new BadRequestException('Provided email already exists.')
 
     const user = await this.UserRepository.create(registerDto)
-    const { password, ...result } = user.toJSON()
+    const { password: _, ...result } = user.toJSON()
 
     return result
+  }
+
+  async login({ email, password }: LoginDto) {
+    const registeredUser = await this.UserRepository.findOne({ email }, {}, { lean: true })
+    if (!registeredUser) throw new BadRequestException('Provided email does not exist.')
+
+    const passwordMatches = compareSync(password, registeredUser.password)
+    if (!passwordMatches) throw new BadRequestException('Provided password is incorrect.')
+
+    const token = sign({ id: registeredUser._id }, this.configService.get('JWT_SECRET'), { expiresIn: '24h' })
+    const { password: _, ...user } = registeredUser
+
+    return { user, token }
   }
 }
